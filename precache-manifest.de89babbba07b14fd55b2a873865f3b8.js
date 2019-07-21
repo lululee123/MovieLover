e419c4a59db07142fe8b564092d5e5b70b8d3630
lululee123 <lulululu123456123456@gmail.com>
2019-07-21 11:19:50 +0800

    Updates

---
+++ precache-manifest.de89babbba07b14fd55b2a873865f3b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b882c800e8616bbd0a91d0593fc79365",
+    "url": "/MovieLover/index.html"
+  },
+  {
+    "revision": "3190affb404463b2211a",
+    "url": "/MovieLover/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "8170921dcf8b8077b724",
+    "url": "/MovieLover/static/css/main.7f7febd1.chunk.css"
+  },
+  {
+    "revision": "3190affb404463b2211a",
+    "url": "/MovieLover/static/js/2.91f50829.chunk.js"
+  },
+  {
+    "revision": "8170921dcf8b8077b724",
+    "url": "/MovieLover/static/js/main.a8537379.chunk.js"
+  },
+  {
+    "revision": "2cd9712871608e972a69",
+    "url": "/MovieLover/static/js/runtime~main.9e35bc98.js"
+  },
+  {
+    "revision": "255e657a1b6b67c2ffde6c24b7ce4802",
+    "url": "/MovieLover/static/media/menu.255e657a.svg"
+  },
+  {
+    "revision": "086279937c806f1e22661538c0ec1b86",
+    "url": "/MovieLover/static/media/oscars.08627993.svg"
+  },
+  {
+    "revision": "617303ab3bd3c369abded95cfd35f95c",
+    "url": "/MovieLover/static/media/pencil.617303ab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.db88a871d739e9a1d954a776588acc8f.js"
+  "/MovieLover/precache-manifest.de89babbba07b14fd55b2a873865f3b8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MovieLover/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
